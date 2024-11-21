7e98559df0d4fc1d8b633596232a190ac03a85ec
DanFleming <flemingdaniel@rocketmail.com>
2024-11-21 23:55:42 +0100

    updated scripts and yaml

--- src/data/normalize_data.py
+++ src/data/normalize_data.py
@@ -4,9 +4,18 @@ from sklearn.preprocessing import StandardScaler
 X_train = pd.read_csv('data/processed_data/X_train.csv')
 X_test = pd.read_csv('data/processed_data/X_test.csv')
 
+if 'date' in X_train.columns:
+    reference_date = pd.to_datetime('2000-01-01')  # Reference date can be anything you want
+    X_train['date'] = pd.to_datetime(X_train['date'])
+    X_train['date'] = (X_train['date'] - reference_date).dt.days
+    
+    X_test['date'] = pd.to_datetime(X_test['date'])
+    X_test['date'] = (X_test['date'] - reference_date).dt.days
+
 scaler = StandardScaler()
+
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-pd.DataFrame(X_train_scaled, columns=X_train.columns).to_csv('data/processed/X_train_scaled.csv', index=False)
-pd.DataFrame(X_test_scaled, columns=X_test.columns).to_csv('data/processed/X_test_scaled.csv', index=False)
+pd.DataFrame(X_train_scaled, columns=X_train.columns).to_csv('data/processed_data/X_train_scaled.csv', index=False)
+pd.DataFrame(X_test_scaled, columns=X_test.columns).to_csv('data/processed_data/X_test_scaled.csv', index=False)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,15 +1,14 @@
-import pandas as pd
-import pickle
+import joblib
 from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+best_params = joblib.load('models/best_params.pkl')
 
 X_train_scaled = pd.read_csv('data/processed_data/X_train_scaled.csv')
 y_train = pd.read_csv('data/processed_data/y_train.csv')
 
-with open('models/best_params.pkl', 'rb') as f:
-    best_params = pickle.load(f)
-
 model = LinearRegression(**best_params)
+
 model.fit(X_train_scaled, y_train)
 
-with open('models/trained_model.pkl', 'wb') as f:
-    pickle.dump(model, f)
+joblib.dump(model, 'models/best_model.pkl')  # Save the trained model here
